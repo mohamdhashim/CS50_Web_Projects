4cd17ae157eafe1b8b89a7b09a7491647bf6e194
Mohamd Zezo <mohamdmido2011@gmail.com>
2021-08-20 15:45:36 +0200

    Building a project skeleton

---
+++ Wiki/encyclopedia/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import markdown 
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def topic(request,name):
+    with open (f'entries/{name.capitalize()}.md','r') as f:
+        text = f.read()
+        html = markdown.markdown(text)
+    
+    name = name.lower() #to prevent making another Html copy if user input Url capitalized ex : wiki/Git same as wiki/git
+    with open(f'encyclopedia/templates/encyclopedia/{name}.html','w') as f:
+        f.write(html)
+
+    return render(request,f'encyclopedia/{name}.html') 
