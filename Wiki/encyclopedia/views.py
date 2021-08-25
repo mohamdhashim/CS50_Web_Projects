4ac4f28e9662a19a4e01c928a8e0e286bbf1256d
Mohamd Zezo <mohamdmido2011@gmail.com>
2021-08-25 04:16:01 +0200

    added search funtion

--- Wiki/encyclopedia/views.py
+++ Wiki/encyclopedia/views.py
@@ -10,9 +10,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
 def topic(request,name):
     if(name.lower() not in util.list_entries()):
         return render(request,'encyclopedia/not_found.html',{'name':name})
+
     with open (f'entries/{name.lower()}.md','r') as f:
         text = f.read()
         html = markdown.markdown(text)
@@ -22,3 +24,27 @@ def topic(request,name):
         f.write(html)
 
     return render(request,f'encyclopedia/{name}.html') 
+
+
+def search(request):
+
+    q = request.GET['q']
+    if q == '':
+        return index(request) #if searching is empty say in the same page
+
+
+    if q.lower() in util.list_entries():
+        return topic(request, q)
+    
+    else: 
+        searched_list = []
+        for entry in util.list_entries():
+            if q in entry:
+                searched_list.append(entry)
+        
+        return render(request, "encyclopedia/search_results.html", {
+        "entries": searched_list ,"count" : len(searched_list)
+        })
+
+    
+   #return render(request,f'encyclopedia/search_results.html') 
