07d0296eb41706d2dae7c1abb244084e0801e4ac
Mohamd Zezo <mohamdmido2011@gmail.com>
2021-08-25 02:10:27 +0200

    adding search_results URL page

--- Wiki/encyclopedia/urls.py
+++ Wiki/encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("wiki", views.index, name="index"),
+    path("wiki/search_results", views.search, name = "search"),
     path("wiki/<str:name>", views.topic, name="topic")
+    
 ]
