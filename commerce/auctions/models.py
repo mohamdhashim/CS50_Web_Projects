c901c7cba586cbf52342b0ca5e9839cee7ff5169
mohamdzezo <mohamdmido2011@gmail.com>
2021-09-18 20:31:55 +0200

    creating  DB models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.expressions import Value
 
 
 class User(AbstractUser):
     pass
+
+class listings(models.Model):
+    title = models.CharField(max_length=30)
+    current_bid = models.IntegerField(null=True)
+    start_bid = models.IntegerField(default=1)
+    picture = models.FilePathField(default = '')
+
+
+class comments(models.Model):
+    user = models.CharField(max_length=30)
+    title_id = models.ForeignKey(listings, on_delete=models.CASCADE,related_name='comments')
+    text = models.CharField(max_length = 500)
+
+class bids(models.Model):
+    tile = models.ForeignKey(listings, on_delete = models.CASCADE, related_name ='bids')
+    user = models.CharField(max_length = 30)
+    value = models.IntegerField(null=False)
+
